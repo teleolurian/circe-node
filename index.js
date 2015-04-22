475df87c3d9724ea71a31cae8fda61af81cab2f7
Sekeol Kim <sekkim@groupon.com>
2015-04-22 09:30:46 -0700

    Sister mercy

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var yiff = require('yahoo-finance');
+var FIELDS = ['a','b','v','c','c1','p2','p','o'];
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+app.post('/quote', function(request, response) {
+  var symbol = request.body.item.message.message.split(' ')[1];
+  yiff.snapshot({
+    fields: FIELDS,
+    symbol: symbol
+  }).then(function (snapshot){
+    var message = '$' + symbol + ': $' + snapshot.ask + '( ' + snapshot.change + ' / ' + snapshot.changeInPercent + '% )';
+
+    response.send(JSON.stringify({
+      color: 'black',
+      message: message,
+      notify: false,
+      message_format: 'text'
+    }));
+  });
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
